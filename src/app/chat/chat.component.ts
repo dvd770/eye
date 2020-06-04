3e03d7c0472eaaf55e7b0aa8d41db29aff78f44d
dvd770 <tty255@gmail.com>
2020-06-04 14:08:14 +0300

    chat scroll to bottom

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { Chat } from './chat'
-
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
+
   chat: any[] = [];
   name: string = 'זמני';
+  scrollContainer: any;
+
+  @ViewChild('scrollDown', { static: true }) scrollDown: ElementRef;
+  @ViewChildren('chat') itemElements: QueryList<any>;
+
+
 
   constructor(private service: ChatService) { }
 
@@ -19,11 +25,26 @@ export class ChatComponent implements OnInit {
       .subscribe(response => {
         this.chat = response.json();
       });
+
   };
 
-  createMessage(input: HTMLInputElement) {
-    let msg = { message: input.value };
-    input.value = '';
+  ngAfterViewInit() {
+    this.scrollContainer = this.scrollDown.nativeElement;
+    this.itemElements.changes.subscribe(_ => this.scrollToBottom());
+  }
+
+
+
+  private scrollToBottom(): void {
+    this.scrollContainer.scroll({
+      top: this.scrollContainer.scrollHeight,
+      behavior: 'smooth'
+    });
+  }
+
+  createMessage(message: { value: string; }) {
+    let msg = { msg: message.value };
+    message.value = '';
 
     this.service.createMessage(msg)
       .subscribe(response => {
@@ -31,6 +52,9 @@ export class ChatComponent implements OnInit {
         this.chat.push(msg);
       })
   };
+
+
+
 
   /*  get chatSorted(){
       return this.chat$.sort((a, b) => new Date(b.CREATE_TS).getTime() -
